782c0933a364fe702176b5c2cf86b32066209ea1
Bivshtex007 <103932684+Bivshtex007@users.noreply.github.com>
2024-07-27 03:40:07 +0300

    Add files via upload

---
+++ modulo_1.cpp
@@ -0,0 +1,19 @@
+//MODULO 1
+using namespace std;
+#include <cmath>
+#include <iostream>
+
+int main(){
+
+	int number;
+	cout << "Enter your INT number" << endl;
+	cin >> number;
+	if (number % 2 == 0){
+		cout << "Your number is EVEN" << endl;
+		}
+	else if (number % 2 == 1) {
+		cout << "Your number is ODD" << endl;
+		}
+
+return 0;
+}
